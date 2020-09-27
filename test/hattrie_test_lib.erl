833938c5e5281f6c052bab81fc681e806e56a06e
Zaiming Shi <zmstone@gmail.com>
2020-09-27 20:21:32 +0200

    Replace '/' in base64 encoding with '-'
    So that we can use '/' as delimiter in tests.

--- test/hattrie_test_lib.erl
+++ test/hattrie_test_lib.erl
@@ -6,17 +6,19 @@ generate_levels(Levels) ->
   generate_levels("", Levels, []).
 
 %% generate levels of random segments,
-%% segments are delimited with '#'
+%% segments are delimited with '/'
 generate_levels(Prefix, [], Acc) ->
   [Prefix | Acc];
 generate_levels(Prefix, [N | Levels], Acc) ->
   lists:foldl(
     fun(_, AccIn) ->
-        generate_levels([Prefix, "#", random()], Levels, AccIn)
+        generate_levels([Prefix, "/", random()], Levels, AccIn)
     end, Acc, lists:seq(1, N)).
 
 %% generate 1-32 random (printable) bytes
 random() ->
   N = rand:uniform(22),
-  hd(binary:split(base64:encode(crypto:strong_rand_bytes(N)), <<"=">>, [trim])).
+  Bin0 = base64:encode(crypto:strong_rand_bytes(N)),
+  Bin = binary:replace(Bin0, <<"/">>, <<"-">>, [global]),
+  hd(binary:split(Bin, <<"=">>, [trim])).
 
